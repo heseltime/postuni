c5a9d64fe2770bc9508841dbf7740d4ae3dcb12a
Jack Heseltine <66922223+heseltime@users.noreply.github.com>
2023-09-08 00:33:11 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="bg-gray-500 text-gray-100 text-center pt-12 pb-2 px-2">
+    <h3 class="text-4xl uppercase mb-4 font-mono text-gray-400">"PostCity Theme" Der Standard Mock-Up</h3>
+    <h4 class="text-md text-gray-400"> This is a mock-up for scrollytelling the Founding Lab Story at Ars Electronica Festival 2023. </h4>
+
+    <h5 class="text-md text-gray-400"> Delivered by the Reporters Team to Der Standard on September 11, 2023. </h5>
+
+    <div class="max-w-3xl w-full mx-auto px-8 pt-32">
+    <div class="pb-64 text-center">
+      <!--<NuxtLink v-for="page in pages" :key="page" :to="page" class="underline mx-4">
+        <span class="main-link" v-if="page == '/'">PostCity</span>
+        <span class="secondary-link" v-else>+ {{ page }}</span>
+      </NuxtLink>-->
+
+      <h2 class="text-5xl uppercase mb-4 font-mono">Welcome to PostCity</h2>
+
+    </div>
+    <Nuxt />
+
+      <!--<div
+        class="fixed left-0 w-full h-0 border-t-2 border-dashed border-black"
+        :style="{ top: triggerTop + 'px' }"
+      >
+        <p>Offset: {{ offset }}</p>
+      </div>-->
+    </div>
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pages: ['/', 'Basic', 'Progress', 'StickyGraphic1', 'StickyGraphic2'],
+      triggerTop: 0,
+      offset: 0.6
+    }
+  },
+  mounted () {
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize()
+  },
+  methods: {
+    handleResize () {
+      this.triggerTop = this.offset * window.innerHeight
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-link {
+  font-size: 2em;
+}
+
+.secondary-link {
+  color: #bbb;
+  text-decoration: none;
+}
+</style>
