a316cb45aa8798dadbdbc230891e090fa00d0016
Jack Heseltine <heseltine@wolfram.com>
2024-10-08 11:08:32 +0200

    Add Banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="bg-gray-500 text-gray-100 text-center pt-12 pb-2 px-2">
-    <h3 class="text-4xl uppercase mb-4 font-mono text-gray-400">"PostCity Theme" Der Standard Mock-Up</h3>
-    <h4 class="text-md text-gray-400"> This is a mock-up for scrollytelling the Founding Lab Story at Ars Electronica Festival 2023. </h4>
+  <div class="bg-gray-500 text-gray-100 text-center pb-2">
+    <div class="w-full background-banner">
+      
+      <h3 class="text-4xl uppercase mb-4 font-mono text-white-400 pt-12">Let's get this right for Linz in 2024</h3>
 
-    <h5 class="text-md text-gray-400"> Delivered by the Reporters Team to Der Standard on September 11, 2023. </h5>
+      <h2 class="text-5xl uppercase mb-4 font-mono pt-30">PostCity x IT-U (postuni)</h2>
 
-    <div class="max-w-3xl w-full mx-auto px-8 pt-32">
+    </div>
+
+    <div class="max-w-3xl w-full mx-auto px-8 pt-10">
     <div class="pb-64 text-center">
       <!--<NuxtLink v-for="page in pages" :key="page" :to="page" class="underline mx-4">
         <span class="main-link" v-if="page == '/'">PostCity</span>
         <span class="secondary-link" v-else>+ {{ page }}</span>
       </NuxtLink>-->
 
-      <h2 class="text-5xl uppercase mb-4 font-mono">Welcome to PostCity</h2>
+      <h3 class="text-4xl uppercase mb-4 font-mono text-white-400 pt-12">What is this about and why now?</h3>
+
+      <h5 class="text-md text-white-400">Now that the city <a href="https://www.nachrichten.at/politik/landespolitik/stadt-linz-zieht-bei-der-umwidmung-fuer-die-itu-die-notbremse;art383,3989606?utm_source=browser&utm_medium=push-notification&utm_campaign=cleverpush-1728293690">has backtracked</a>, rightfully, on the planned Dornach "Grüngürtel" extension, here is another chance for the PostCity location.</h5>
+
+      <h4 class="text-md text-gray-400">Don't put short-term, boring business-as-usual before a truly transformative project opportunity, with, yes, business value as well, dear <b>Österreichische Post</b>* -</h4>
+
+      <h4 class="text-md text-gray-400">*Current Owner of PostCity</h4>
+
+      <h4 class="text-md text-gray-400">If Linz wants to continue to be a creative hub, it's <b>time to put the money where the mouth is</b>. </h4>
+      
+      <h4 class="text-md text-gray-400">This requires making yourself heard by city and government officials as well as private businesspeople owning the property in question.</h4>
 
     </div>
     <Nuxt />
@@ -23,10 +36,21 @@
       >
         <p>Offset: {{ offset }}</p>
       </div>-->
+
+    <div class="pb-64 text-center">
+      <!--<NuxtLink v-for="page in pages" :key="page" :to="page" class="underline mx-4">
+        <span class="main-link" v-if="page == '/'">PostCity</span>
+        <span class="secondary-link" v-else>+ {{ page }}</span>
+      </NuxtLink>-->
+
+      <h3 class="text-4xl uppercase mb-4 font-mono text-white-400 pt-12">Call to Action</h3>
+
+      
+
+    </div>
+
     </div>
   </div>
-
-  
 </template>
 
 <script>
@@ -58,5 +82,12 @@ export default {
 .secondary-link {
   color: #bbb;
   text-decoration: none;
+}
+
+.background-banner {
+  background-image: url('@/assets/img/postuni.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 600px;
 }
 </style>
